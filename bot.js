9e13b342512daa5818bc846d572d8c48055ef440
Michael C <michael@mchang.name>
2020-12-12 00:16:31 -0500

    initial commit

---
+++ bot.js
@@ -0,0 +1,69 @@
+// imports
+require('dotenv').config()
+require('enve')
+const Discord = require('discord.js')
+const strats = require('./strats.json') // strings
+
+// variables
+var map, side
+const valMaps = ["split", "haven", "bind", "icebox"]
+const valSides = ["attack", "defense"]
+
+const client = new Discord.Client() // create client
+client.on('ready', () => {
+  console.log('Ready')
+  client.user.setPresence({ // set presence
+    activity: { type: process.enve.ACT_TYPE, name: process.enve.ACT_NAME },
+    status: process.enve.STATUS
+  })
+})
+client.login(process.enve.TOKEN) // login
+
+client.on('message', message => {
+  const prefix = process.enve.PREFIX // set prefix
+  if (!message.author.bot && message.content.startsWith(prefix)) { // check if sent by self & check for prefix
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift().toLowerCase();
+    var response
+    // run appropiate command
+    if (command === 'map') { // set map
+      if (valMaps.includes(args[0])) {
+        map = args[0]
+        response = `Map set to ${map}`
+      } else { response = "Invalid Map"
+      }
+    } else if (command === 'side') { // atk or def
+        if (valSides.includes(args[0])) {
+            side = arg[0]
+            response = `Side set to ${side}`
+        } else { response = "Invalid Side"
+        }
+    } else if (command === 'strat') { // get new strat
+        response = {embed: generateStrat()}
+    } else if (command === 'help') { //
+        response = 'commands: map/side/strat'
+    } else { response = {embed: generateStrat()}
+    }
+    message.channel.send(response)
+}})
+
+function generateStrat() {
+    const newStrat = randomize(getStrat(map, side))
+    var stratEmbed = new Discord.MessageEmbed()
+    stratEmbed.fields = [
+        {name: 'Map', value: `${map}`, inline: true},
+        {name: 'Side', value: `${side}`, inline: true},
+        {name: `${newStrat[0]}`, value: `${newStrat[1]}`}]
+    return stratEmbed
+}
+
+function getStrat(map, side) {
+    const fetchString = `${map}_${side}`
+    return {...strats.universal, ...strats[side], ...strats[map], ...strats[fetchString]}
+}
+
+function randomize(arr) {
+    var keys = Object.keys(arr);
+    const select = keys[keys.length * Math.random() << 0] 
+    return [select, arr[select]];
+}
